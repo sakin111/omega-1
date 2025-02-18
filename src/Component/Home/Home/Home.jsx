2268d6656df18bc4b6602a3c83ef7860d877ff71
sakin111 <maliksakin53@gmail.com>
2025-02-18 14:03:41 +0600

    .

--- src/Component/Home/Home/Home.jsx
+++ src/Component/Home/Home/Home.jsx
@@ -4,7 +4,11 @@ import Section2 from "./Section2/Section2";
 // import Section1 from "./Section1/Section1";
 import Section3 from "./Section3/Section3";
 import Slider from "./Slider/Slider";
-import SliderText from "./Slider/Slidertext";
+import TextAnimation from "./Slider/TextAnimation";
+
+
+
+
 
 
 
@@ -15,7 +19,8 @@ const Home = () => {
     return (
         <div className="bg-white">
             <Banner></Banner>
-            <SliderText></SliderText>
+
+           <TextAnimation></TextAnimation>
             <Slider></Slider>
            
             <Section2></Section2>
---
+++ src/Component/Home/Home/Slider/TextAnimation.jsx
@@ -0,0 +1,13 @@
+
+
+const TextAnimation = () => {
+
+
+    return (
+        <div>
+            <h1 className="text-6xl py-16 mt-7 text-center time" data-aos="fade-up">Our Most Popular Hotel Across The World</h1>
+        </div>
+    );
+};
+
+export default TextAnimation;
