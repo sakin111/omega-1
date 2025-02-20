03fe436fd3a1adca9e5c146e43425c1ae7346c6f
sakin111 <maliksakin53@gmail.com>
2025-02-20 13:46:31 +0600

    .

--- src/Hook/useAxiosPublic.jsx
+++ src/Hook/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const baseURL = process.env.NODE_ENV === "production"
-  ? 'https://new-project-server-git-main-maliksakin53gmailcoms-projects.vercel.app/'
+  ? 'https://new-project-server-git-main-maliksakin53gmailcoms-projects.vercel.app'
   : 'http://localhost:5000';
 
 const axiosPublic = axios.create({
